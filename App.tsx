801654882f920603546e94a37f2c34987c8fc82d
akhil683 <onkar.palsra@gmail.com>
2024-02-13 15:40:15 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  View, 
+  Text,
+  // SafeAreaView
+} from "react-native";
+
+const App = () =>
+ {
+  return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+  )
+}
+export default App;
