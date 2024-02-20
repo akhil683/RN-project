53c21a80e7a272c4f1c82870d898a71be3856565
akhil683 <onkar.palsra@gmail.com>
2024-02-21 01:20:06 +0530

    inital commit - Password generator

--- App.tsx
+++ App.tsx
@@ -1,26 +1,42 @@
+import React, { useState } from 'react';
 import { View, Text, SafeAreaView, ScrollView } from 'react-native';
-import React from 'react';
-import FlatCards from './components/FlatCards';
-import ElevatedCards from './components/ElevatedCards';
-import FancyCard from './components/FancyCard';
-import ActionCard from './components/ActionCard';
-import ContactList from './components/ContactList';
+
+import * as Yup from "yup";
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+  .min(4, "Should be min of 4 characters")
+  .max(16, "Should be max of 16 characters")
+  .required("Length is required")
+})
 
 const App = () => {
+
+  const [password, setPassword ] = useState("")
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+  const [ lowerCase, setLowerCase ] = useState(false)
+  const [ numbers, setNumbers] = useState(false);
+  const [ symbols, setSymbols] = useState(false);
+
+  const generatedPasswordString = (passwordLength: number) => {
+    
+  }
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = ""
+    for (let i =0; i < password.length; i++) {
+      const characterIndex = Math.round(Math.random()*characters.length);
+      result += characters.charAt(characterIndex)
+    }
+    return result; 
+  }
+  const resetPassword = () => {
+
+  }
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <FlatCards />
-          <ElevatedCards />
-          <FancyCard />
-          <ContactList />
-          <ActionCard />
-
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-
+  <View>
+    <Text>Hello World</Text>
+  </View>  
   )
 }
 export default App
